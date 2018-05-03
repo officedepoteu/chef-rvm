dd9b6cc9c00e998885f05ad19cd245b93c39f629
Peter Hayes <peter.hayes@officedepot.eu>
2018-05-03 15:42:57 +0100

    Move zlib-devel and readline-devel down the install order

--- providers/ruby.rb
+++ providers/ruby.rb
@@ -135,17 +135,17 @@ def install_ruby_dependencies(rubie)
                     ncurses-dev automake libtool bison ssl-cert pkg-config libgdbm-dev libffi-dev}
         pkgs += %w{ subversion }  if rubie =~ /^ruby-head$/
       when "suse"
-        pkgs = %w{ gcc-c++ patch zlib-devel libffi-devel
-                   sqlite3-devel libxml2-devel libxslt-devel }
+        pkgs = %w{ gcc-c++ patch libffi-devel sqlite3-devel libxml2-devel libxslt-devel }
         if node['platform_version'].to_f >= 12.1
-          pkgs += %w{ libz1 libreadline6 readline-devel libopenssl-devel }
+          pkgs += %w{ libz1 libreadline6 libopenssl-devel }
         elsif node['platform_version'].to_f >= 12.0
-          pkgs += %w{ zlib libreadline6 readline-devel libopenssl-devel }
+          pkgs += %w{ zlib libreadline6 libopenssl-devel }
         elsif node['platform_version'].to_f >= 11.0
-          pkgs += %w{ zlib libreadline5 readline-devel libopenssl-devel }
+          pkgs += %w{ zlib libreadline5 libopenssl-devel }
         else
-          pkgs += %w{ zlib readline readline-devel openssl-devel }
+          pkgs += %w{ zlib readline openssl-devel }
         end
+        pkgs += %w{ zlib-devel readline-devel }
         pkgs += %w{ git subversion autoconf } if rubie =~ /^ruby-head$/
       when "centos","redhat","fedora","scientific","amazon"
         pkgs = %w{ gcc-c++ patch readline readline-devel zlib zlib-devel
